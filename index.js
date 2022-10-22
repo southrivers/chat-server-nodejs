f4dbd321d4e98a3f2f3f21e39ee92cb6794a1a48
wangershuai <wanganlou@gmail.com>
2022-10-22 21:03:55 +0800

    初始化服务端,并用于事件的接收

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express')
 
+
 const app = express();
 
+// 这里用于请求的处理
 app.get('/hello', function(req, res) {
     res.send('hello world')
 })
-
 // 启动服务器监听
-app.listen(8080, () => {
+const server = app.listen(8080, () => {
     console.log('server is start!')
 })
+
+
+// 此处用于消息的收发
+const ScoketServer = require('socket.io')
+const io = ScoketServer(server)
+// 这里用于收发消息
+io.on('connection', (socket) => {
+    console.log(socket)
+})
+
+
